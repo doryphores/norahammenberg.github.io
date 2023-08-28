9217035dd98f4d83c5bf6f89530def5bf99184b2
Martin Laine <martin.laine@gmail.com>
2023-08-28 14:11:00 +0100

    Changed the header to include "Hello"

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import '../styles/header.scss';
 function Header(props) {
     return(
         <div className='headerWrapper'>
-            <h1 className={props.className}>{props.text}</h1>
+            <h1 className={props.className}>{props.text} Hello</h1>
         </div>
     );
 }
